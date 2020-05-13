a0926f1469036cee6717054ce1bcdb3719fc405f
vxlter_smxth <2933983+vxltersmxth@users.noreply.github.com>
2020-05-13 15:18:41 +0300

    remove user

--- jupyter_dock_start.sh
+++ jupyter_dock_start.sh
@@ -1,4 +1,4 @@
 DATA_PATH=/media/andrey/slow/
 HOST_PORT=8890
 
-docker run -it --init --ipc=host --user="$(id -u):$(id -g)" --volume="$DATA_PATH:/drive/" -p $HOST_PORT:8888 -e NVIDIA_VISIBLE_DEVICES=all anaconda_dock jupyter notebook /drive/ --ip 0.0.0.0 --port 8888 --no-browser --allow-root
+docker run -it --init --ipc=host --volume="$DATA_PATH:/drive/" -p $HOST_PORT:8888 -e NVIDIA_VISIBLE_DEVICES=all anaconda_dock jupyter notebook /drive/ --ip 0.0.0.0 --port 8888 --no-browser --allow-root
